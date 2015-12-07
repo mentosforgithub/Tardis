ed2fb8e1aa4716c8e24e3baa45a8cdf2c5bfe9e4
mentosforgithub <mentos.vicky@gmail.com>
2015-12-07 09:50:43 -0500

    Update pay.php

--- pay.php
+++ pay.php
@@ -63,6 +63,11 @@ while(empty($arr_paycart))
 echo "Your order_ID is($orderID_insert)";
 echo "The total money to pay is{$total_money}";
 
+unset($_SESSION["mycar"]);
+unset($_SESSION["sales_result"]);
+
+ob_clean();
+
 
 
 
